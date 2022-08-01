dd5fc96a463543c2522ee65e3923ce8db2b24401
John Thompson <john@springframework.guru>
2022-08-01 12:15:34 -0400

    adding lombok pojo example

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6restmvc.model;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
