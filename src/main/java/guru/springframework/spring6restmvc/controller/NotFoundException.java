642dd0afcb691e3780917a6b87c072e146025751
John Thompson <john@springframework.guru>
2022-08-13 11:22:50 -0400

    adding response status example

--- src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/NotFoundException.java
@@ -1,8 +1,12 @@
 package guru.springframework.spring6restmvc.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by jt, Spring Framework Guru.
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
