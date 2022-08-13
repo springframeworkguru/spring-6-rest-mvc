12b7292103ac239559d68314db7fc755f9e5a877
John Thompson <john@springframework.guru>
2022-08-13 15:46:53 -0400

    adding UUID generation

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -1,10 +1,9 @@
 package guru.springframework.spring6restmvc.entities;
 
 import guru.springframework.spring6restmvc.model.BeerStyle;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,6 +21,9 @@ import java.util.UUID;
 public class Beer {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
 
     @Version
--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring6restmvc.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,6 +19,9 @@ import java.util.UUID;
 public class Customer {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     private String name;
 
