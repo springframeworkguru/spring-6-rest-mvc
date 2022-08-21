3d4eadc1c6bf904845b4c2209ceb11263b1dfabf
John Thompson <john@springframework.guru>
2022-08-21 12:48:26 -0400

    adding JPA validation

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6restmvc.entities;
 
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -28,10 +30,20 @@ public class Beer {
 
     @Version
     private Integer version;
+
+    @NotNull
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +20,12 @@ class BeerRepositoryTest {
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.PALE_ALE)
+                        .upc("234234234234")
+                        .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
