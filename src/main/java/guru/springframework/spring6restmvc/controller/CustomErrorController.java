830efe4c2c7c4a29a5bdfc4fc17d916bdc1ff053
John Thompson <john@springframework.guru>
2022-08-21 13:14:45 -0400

    adding JPA violation Exception handler

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6restmvc.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,11 @@ import java.util.stream.Collectors;
  */
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception){
+        return ResponseEntity.badRequest().build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception){
