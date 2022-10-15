1e5eff7a44a56bfa876026328e44195a8339b87e
John Thompson <john@springframework.guru>
2022-10-15 14:05:23 -0400

    adding spring data jpa method

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -3,10 +3,13 @@ package guru.springframework.spring6restmvc.repositories;
 import guru.springframework.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,22 +1,34 @@
 package guru.springframework.spring6restmvc.repositories;
 
+import guru.springframework.spring6restmvc.bootstrap.BootstrapData;
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeerNameTooLong() {
