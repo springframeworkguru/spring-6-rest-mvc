df5ec52633c253850cd4a12842f40b77798e358e
John Thompson <john@springframework.guru>
2022-12-26 11:26:08 -0500

    adding sec config

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,7 +2,6 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,8 +15,8 @@ public class SpringSecConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .anyRequest().authenticated()
-                .and().httpBasic(Customizer.withDefaults())
-                .csrf().ignoringRequestMatchers("/api/**");
+                .and().oauth2ResourceServer().jwt();
+
         return http.build();
     }
 
