a84e43d384ad83ffacc9e519259bf63f4e5494e4
John Thompson <john@springframework.guru>
2023-01-21 16:05:41 -0500

    adding test

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,12 +2,14 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Profile("!test")
 @Configuration
 public class SpringSecConfig {
 
--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -6,14 +6,34 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
 public class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests().anyRequest().permitAll();
+
+            return http.build();
+        }
+    }
 
     @LocalServerPort
     Integer localPort;
