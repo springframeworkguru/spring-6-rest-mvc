3036108be3e3fbc90e29ce78e24bd7e647c3f362
John Thompson <john@springframework.guru>
2023-01-21 16:28:01 -0500

    adding whitelist rule

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.controller;
 
 import com.atlassian.oai.validator.OpenApiInteractionValidator;
 import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
+import com.atlassian.oai.validator.whitelist.ValidationErrorsWhitelist;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +17,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 
+import static com.atlassian.oai.validator.whitelist.rule.WhitelistRules.messageHasKey;
 import static io.restassured.RestAssured.given;
 
 /**
@@ -29,6 +31,9 @@ public class BeerControllerRestAssuredTest {
 
     OpenApiValidationFilter filter = new OpenApiValidationFilter(OpenApiInteractionValidator
             .createForSpecificationUrl("oa3.yml")
+            .withWhitelist(ValidationErrorsWhitelist.create()
+                    .withRule("Ignore date format",
+                    messageHasKey("validation.response.body.schema.format.date-time")))
             .build());
 
     @Configuration
