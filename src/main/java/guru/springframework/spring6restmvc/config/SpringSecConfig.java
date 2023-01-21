6f773222d19fb7ec20ef5e9289b3e6ce20280d47
John Thompson <john@springframework.guru>
2023-01-21 14:14:06 -0500

    adding maven deps

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -14,6 +14,8 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
+                .requestMatchers("/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().oauth2ResourceServer().jwt();
 
