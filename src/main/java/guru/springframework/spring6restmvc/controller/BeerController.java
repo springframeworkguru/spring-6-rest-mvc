7a7e3258c7017c015851afa0350b36e360c90568
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-22 13:17:21 +0300

    Created the controller and annotated it with @AllArgsConstructor to automatically generate the Constructor using lombok

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+}
