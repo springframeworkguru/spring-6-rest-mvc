2abe992aaddcbd319f09d612e2e7f06fb6eb5bf8
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-22 13:08:16 +0300

    used the @Builder annotation to create our implementation

--- src/main/java/guru/springframework/spring6restmvc/model/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/model/Beer.java
@@ -1,10 +1,12 @@
 package guru.springframework.spring6restmvc.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
+@Builder
 @Data
 public class Beer {
     private UUID id;
--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,11 +2,22 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
-        return null;
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy Cat")
+                .upc("123456")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
     }
 }
