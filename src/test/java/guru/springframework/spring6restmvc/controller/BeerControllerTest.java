6eb06439294788aadeb5335772cd19674f882b89
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-23 22:23:46 +0300

    Creted a test for the Beer Controller model

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6restmvc.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerControllerTest {
+
+    @Test
+    void getBeerById() {
+    }
+}
