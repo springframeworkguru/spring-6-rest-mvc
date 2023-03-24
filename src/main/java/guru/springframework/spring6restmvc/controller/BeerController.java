45693265efb55043fc069674de922dcc0c4da5f3
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-24 12:17:21 +0300

    I did set up the base mapping for every method

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -5,7 +5,9 @@ import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,20 +17,21 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET )
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    @RequestMapping("/api/v1/beer/{beerId}")
-    public Beer getBeerById(UUID id){
+    @RequestMapping("/api/v1/beer/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by Id in Controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
 
     }
 }
