5adf3833eda5f72efc9a3db9b43b3e34b320e02d
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-24 11:32:05 +0300

    Modified the BeerService Class to pull the new list method to its interface

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -9,8 +9,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
 
-import static guru.springframework.spring6restmvc.model.BeerStyle.PALE_ALE;
-
 @Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
@@ -60,6 +58,7 @@ public class BeerServiceImpl implements BeerService {
         beerMap.put(beer3.getId(), beer3);
     }
 
+    @Override
     public List<Beer> listBeers(){
         return new ArrayList<>(beerMap.values());
     }
@@ -67,18 +66,8 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
 
-        log.debug("Get Beer Id in service was called");
+        log.debug("Get Beer Id - in service was called. id is: "+id.toString());
 
-        return Beer.builder()
-                .id(id)
-                .version(1)
-                .beerName("Galaxy Cat")
-                .beerstyle(PALE_ALE)
-                .upc("123456")
-                .price(new BigDecimal("12.99"))
-                .quantityOnHand(122)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+        return beerMap.get(id);
     }
 }
