2ff4abb355d7026afff3470c0ad631ad7cad31ba
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-21 14:21:08 +0300

    Unit tests for JPA repositories

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testSaveCustomer() {
+        Customer customer = customerRepository.save(Customer.builder()
+                                                            .name("Testiasiakas")
+                .build());
+
+        assertThat(customer.getId()).isNotNull();
+    }
+}
