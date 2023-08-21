01bb35739a4f8c7621557789d2c3b0ec271a4799
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-21 14:07:28 +0300

    Created interfaces for JPA repositories

---
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
---
+++ src/main/java/guru/springframework/spring6restmvc/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
