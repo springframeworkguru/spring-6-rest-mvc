43a79a02a7cff757308625745c52d389f2aa6084
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-21 11:09:13 +0300

    Central exception handling using Spring ControllerAdvice and ExceptionHandler

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/ExceptionController.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6restmvc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+}
