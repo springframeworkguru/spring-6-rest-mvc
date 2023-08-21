a0978bd638388a97931509ac93d8a6fb9ca1855f
Soheil Parvizi <soheil.parvizi@gmail.com>
2023-08-21 18:08:31 +0200

    Refactor codes

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.*;
 @Service
 public class BeerServiceImpl implements BeerService {
 
-    private Map<UUID, Beer> beerMap;
+    private final Map<UUID, Beer> beerMap;
 
     public BeerServiceImpl() {
         this.beerMap = new HashMap<>();
