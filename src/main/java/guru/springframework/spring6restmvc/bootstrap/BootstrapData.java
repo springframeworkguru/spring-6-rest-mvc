26cb79af1fe008ff3244f9d2e6faba93d0e6aa64
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-22 11:18:27 +0300

    Initialize bootstrap data

--- src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
@@ -1,2 +1,29 @@
-package guru.springframework.spring6restmvc.bootstrap;public class BootstrapData {
+package guru.springframework.spring6restmvc.bootstrap;
+
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BootstrapData {
+
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerData();
+        loadCustomerData();
+    }
+
+    private void loadBeerData() {
+        System.out.println("Loading Beer data...");
+    }
+
+    private void loadCustomerData() {
+        System.out.println("Loading Customer data...");
+    }
+
 }
