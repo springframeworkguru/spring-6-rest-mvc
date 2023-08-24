52ca397d9bf911d2bbb9574aade54c5a9ef81437
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-24 10:50:50 +0300

    JPA validation error message

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomErrorController.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6restmvc.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +13,28 @@ import java.util.Map;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity handleJPAViolations(TransactionSystemException exception) {
+
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if (exception.getCause().getCause() instanceof ConstraintViolationException) {
+            ConstraintViolationException ve = (ConstraintViolationException) exception.getCause().getCause();
+
+            List errors = ve.getConstraintViolations().stream()
+                                        .map(constraintViolation -> {
+                                            Map<String, String> errMap = new HashMap<>();
+                                            errMap.put(constraintViolation.getPropertyPath().toString(),
+                                                    constraintViolation.getMessage());
+                                            return errMap;
+                                        }).toList();
+
+            return responseEntity.body(errors);
+        }
+
+        return responseEntity.build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity handleBindErrors(MethodArgumentNotValidException exception) {
