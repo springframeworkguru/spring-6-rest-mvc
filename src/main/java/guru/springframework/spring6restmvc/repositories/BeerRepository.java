e93ab57ab158b951ab0cba4ec6ff098dd1fbcb2a
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-28 12:34:49 +0300

    Find by name with Spring Data JPA

--- src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/spring6restmvc/repositories/BeerRepository.java
@@ -3,7 +3,10 @@ package guru.springframework.spring6restmvc.repositories;
 import guru.springframework.spring6restmvc.entities.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 }
