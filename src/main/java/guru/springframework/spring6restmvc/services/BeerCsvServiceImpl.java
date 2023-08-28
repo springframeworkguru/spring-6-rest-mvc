1f63918cd40459a4c6262c5b49a5ee2ec307f619
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-28 09:35:10 +0300

    Removed unused annotation

--- src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -2,7 +2,6 @@ package guru.springframework.spring6restmvc.services;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import guru.springframework.spring6restmvc.model.BeerCSVRecord;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
