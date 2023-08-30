3f5b72133db4d1d104d130c57051dc453f8c092a
Tatu Soininen <tatu.soininen@cinia.fi>
2023-08-30 12:34:46 +0300

    Migration script to introduce new database table

---
+++ src/main/resources/db/migration/V4__category.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS beer_category;
+
+CREATE TABLE category (
+    id                  VARCHAR(36) NOT NULL PRIMARY KEY,
+    description         VARCHAR(50),
+    created_date        TIMESTAMP,
+    last_modified_date  DATETIME(6) DEFAULT NULL,
+    version             BIGINT DEFAULT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE beer_category (
+    beer_id     VARCHAR(36) NOT NULL,
+    category_id VARCHAR(36) NOT NULL,
+    PRIMARY KEY (beer_id, category_id),
+    CONSTRAINT pc_beer_id_fk FOREIGN KEY (beer_id) REFERENCES beer (id),
+    CONSTRAINT pc_category_id_fk FOREIGN KEY (category_id) REFERENCES  category (id)
+) ENGINE = InnoDB;
