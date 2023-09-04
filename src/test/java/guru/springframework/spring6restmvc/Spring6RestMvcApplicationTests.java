1b0f03520538813ad4ec39465cd6cf086521527a
Tatu Soininen <tatu.soininen@cinia.fi>
2023-09-04 11:28:44 +0300

    Simple unit test to see Spring Application Context

--- src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -1,10 +1,23 @@
 package guru.springframework.spring6restmvc;
 
+import guru.springframework.spring6restmvc.controller.TestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6RestMvcApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    void testGetControllerFromCtx() {
+        TestController testController = applicationContext.getBean(TestController.class);
+
+        System.out.println(testController.sayHello());
+    }
 
     @Test
     void contextLoads() {
