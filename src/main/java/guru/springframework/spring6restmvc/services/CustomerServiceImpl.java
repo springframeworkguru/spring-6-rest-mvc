8c4b18b48d1b3b44c76dc3af6841730c3fed918b
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2023-10-23 17:19:49 +0500

    mentor addings

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -49,6 +49,7 @@ public class CustomerServiceImpl implements CustomerService {
     public Customer saveNewCustomer(Customer customer) {
         Customer savedCustomer = Customer.builder()
                 .id(UUID.randomUUID())
+                .version(1)
                 .createdDate(LocalDateTime.now())
                 .lastModifiedDate(LocalDateTime.now())
                 .customerName(customer.getCustomerName())
