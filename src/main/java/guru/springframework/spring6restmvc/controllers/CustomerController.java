779ba0576ac2a0c5ceee0794e03f54e621ba6f6b
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2023-11-06 15:41:39 +0500

    change test beer controller to mockito

--- src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
