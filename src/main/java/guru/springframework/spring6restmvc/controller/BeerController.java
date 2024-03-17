59e23e8878aa2546c00c8acdebce94df9ce7ea67
Jörg-Christian Müller <joerg-christian.mueller@shv-is.eu>
2024-03-17 13:47:26 +0100

    Project Lombok Logging with SLF4J

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -1,15 +1,27 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("Get Beer by Id - in controller");
+
+        return beerService.getBeerById(id);
+    }
 
 }
--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -1,14 +1,12 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
-import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
-@Service
 public interface BeerService {
 
     Beer getBeerById(UUID id);
