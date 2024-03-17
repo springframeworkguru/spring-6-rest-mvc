e326d2b873b245b5c00b0de2f51d2bcb0fa2d949
Jörg-Christian Müller <joerg-christian.mueller@shv-is.eu>
2024-03-17 13:28:03 +0100

    Adding @Service annotation to the interface BeerService

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -1,12 +1,14 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Service
 public interface BeerService {
 
     Beer getBeerById(UUID id);
