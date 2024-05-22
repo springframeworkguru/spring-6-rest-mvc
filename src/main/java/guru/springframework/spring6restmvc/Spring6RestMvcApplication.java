abf3c47955ae61e098773e7877c57248849b7744
John Thompson <john@springframework.guru>
2024-05-22 11:47:06 -0400

    add cache deps

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -2,7 +2,9 @@ package guru.springframework.spring6restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Spring6RestMvcApplication {
 
