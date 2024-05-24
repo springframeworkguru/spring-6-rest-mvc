0baf8e109c6924819b390f3d37b05c1b61e499af
John Thompson <john@springframework.guru>
2024-05-24 13:23:02 -0400

    adding async config

---
+++ src/main/java/guru/springframework/spring6restmvc/config/TaskConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executors;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@EnableAsync
+@Configuration
+public class TaskConfig {
+
+    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+}
--- src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.listeners;
 
 import guru.springframework.spring6restmvc.events.BeerCreatedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,10 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class BeerCreatedListener {
 
+    @Async
     @EventListener
     public void listen(BeerCreatedEvent event) {
         System.out.println("I heard a beer was created!");
         System.out.println(event.getBeer().getId());
+
+        System.out.println("Current Thread Name: " + Thread.currentThread().getName());
+        System.out.println("Current Thread ID: " + Thread.currentThread().getId());
 
         //todo impl - add real implementation to persist audit record
     }
