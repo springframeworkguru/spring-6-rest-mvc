1a272a354859b7c8ec173858a5e8c3440608b988
John Thompson <john@springframework.guru>
2024-05-24 13:54:19 -0400

    add beer audit entity

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerAudit.java
@@ -0,0 +1,79 @@
+package guru.springframework.spring6restmvc.entities;
+
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.*;
+import org.hibernate.type.SqlTypes;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerAudit {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID auditId;
+
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    private Integer version;
+
+    @Size(max = 50)
+    @Column(length = 50)
+    private String beerName;
+
+    private BeerStyle beerStyle;
+
+    @Size(max = 255)
+    private String upc;
+    private Integer quantityOnHand;
+
+    private BigDecimal price;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updateDate;
+
+    @CreationTimestamp
+    private LocalDateTime createdDateAudit;
+
+    private String principalName;
+
+    private String auditEventType;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
