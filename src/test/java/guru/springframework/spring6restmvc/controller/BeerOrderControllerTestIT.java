686b9dcb9d7d75e75ad25c4cf1a19414c33e5e4a
John Thompson <john@springframework.guru>
2024-05-29 16:38:59 -0400

    adding test for create operation

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerTestIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerTestIT.java
@@ -1,6 +1,11 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6restmvc.model.BeerOrderCreateDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderLineCreateDTO;
 import guru.springframework.spring6restmvc.repositories.BeerOrderRepository;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,14 +15,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static guru.springframework.spring6restmvc.controller.BeerControllerTest.jwtRequestPostProcessor;
+import java.util.Set;
 
+import static guru.springframework.spring6restmvc.controller.BeerControllerTest.jwtRequestPostProcessor;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class BeerOrderControllerTestIT {
@@ -28,6 +33,15 @@ class BeerOrderControllerTestIT {
     @Autowired
     BeerOrderRepository beerOrderRepository;
 
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     MockMvc mockMvc;
 
     @BeforeEach
@@ -35,6 +49,27 @@ class BeerOrderControllerTestIT {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac)
                 .apply(springSecurity())
                 .build();
+    }
+
+    @Test
+    void testCreateBeerOrder() throws Exception {
+        val customer = customerRepository.findAll().get(0);
+        val beer = beerRepository.findAll().get(0);
+
+        val beerOrderCreateDTO = BeerOrderCreateDTO.builder()
+                .customerId(customer.getId())
+                .beerOrderLines(Set.of(BeerOrderLineCreateDTO.builder()
+                        .beerId(beer.getId())
+                        .orderQuantity(1)
+                        .build()))
+                .build();
+
+        mockMvc.perform(post(BeerOrderController.BEER_ORDER_PATH)
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(beerOrderCreateDTO))
+                        .with(jwtRequestPostProcessor))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
     }
 
     @Test
