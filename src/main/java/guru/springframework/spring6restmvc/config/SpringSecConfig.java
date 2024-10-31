fbf7d7f3bd4627b774b6598833aae987d04bf182
DIMITRIOS CHRISOS <sdi2100275@di.uoa.gr>
2024-10-31 15:06:47 +0200

    Finished Section 21: Spring Security Basic Auth

---
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Configuration
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic(Customizer.withDefaults())
+                .csrf().ignoringRequestMatchers("/api/**");
+        return http.build();
+    }
+
+}
